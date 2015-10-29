8e651a2f0a6a49b0c3f39bb6301c9b0b674e9f36
lyc110104@sina.com <lyc110104>
2015-10-29 10:40:38 +0800

    xinde

--- app/src/main/java/com/example/smalls/test/MainActivity.java
+++ app/src/main/java/com/example/smalls/test/MainActivity.java
@@ -4,8 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int a;
     int b;
 
@@ -15,4 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
